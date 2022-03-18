30f652743aef49540a2f425f2a0c73b16ebb6e3e
DanielPickens <72703981+DanielPickens@users.noreply.github.com>
2022-03-17 20:34:11 -0600

    updated main.go

--- example/server/main.go
+++ example/server/main.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"net"
 
-	"github.com/DanielPickens/Obsidian/example/pb"
+	"github.com/DanielPickens/Obsidian/example/pd"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
