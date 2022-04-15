ee537fd20d069a183eb894cb18a85eba343b7359
Daniel Pickens <dpickens1989@gmail.com>
2022-04-15 09:16:38 -0600

    Added function 'RunD' as  a convenience function that runs the requested cmd and returns the error based off connection with secure client.

--- cmd.go
+++ cmd.go
@@ -35,6 +35,58 @@ func Execute() error {
 	return rootCmd.Execute()
 }
 
+// RunD is a convenience function that runs the requested cmd and returns the error.
+func runD(cmd *cobra.Command, args []string) error {
+	if len(args) == 0 {
+		return fmt.Errorf("missing method name")
+	}
+
+	if *addr == "" {
+		return fmt.Errorf("missing address")
+	}
+
+	conn, err := grpc.Dial(*addr, grpc.WithInsecure())
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	client := NewObsidianClient(conn)
+
+	methodName := args[0]
+	method := reflect.ValueOf(client).MethodByName(methodName)
+	if !method.IsValid() {
+		return fmt.Errorf("unknown method: %s", methodName)
+	}
+
+	inputType := method.Type().In(2)
+	inputValue := reflect.New(inputType)
+	if *input != "" {
+		if err := json.Unmarshal([]byte(*input), inputValue.Interface()); err != nil {
+			return err
+		}
+	}
+
+	outputType := method.Type().In(3)
+	outputValue := reflect.New(outputType)
+
+	if err := method.Call([]reflect.Value{
+		reflect.ValueOf(context.Background()),
+		inputValue.Elem(),
+		outputValue.Elem(),
+	}); err != nil {
+		return err
+	}
+
+	output, err := json.MarshalIndent(outputValue.Interface(), "", "  ")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(output))
+
+	return nil
+}
 func RunE(
 	method,
 	inT string,
