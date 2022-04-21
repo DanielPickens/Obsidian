806f13d5fb0d29aad30ea51cdee0d2867012c334
Daniel Pickens <dpickens1989@gmail.com>
2022-04-21 03:46:00 -0600

    Added functions 'RunF and RunH' as convenience functions for CLI interoperability

--- cmd.go
+++ cmd.go
@@ -154,6 +154,8 @@ func RunF(
 			if len(result) != 2 {
 				panic("service methods should always return 2 values (error and result)")
 			}
+
+			if len
 			if !result[1].IsNil() {
 				return result[1].Interface().(error)
 			}
@@ -218,80 +220,9 @@ func RunH(
 			}
 			if !result[1].IsNil() {
 				return result[1].Interface().(error)
-func runG( 
-	method string,
-	newClient func(*grpc.ClientConn) interface{},
-) func(*cobra.Command, []string) error {
-	return func(cmd *cobra.Command, args []string) error {
-		conn, err := dial(*addr)
-		if err != nil {
-			return err
-		}
-	func mainmarshall(data []byte, v interface{}) error {
-		return json.Unmarshal(data, v)
-	}
-		defer conn.Close()
-		c := newClient(conn)
-		cv := reflect.ValueOf(c)
-		method := cv.MethodByName(method)
-		if method.IsValid() {
-			in := reflect.New(proto.MessageType(inT).Elem()).Interface()
-			if len(*input) > 0 {
-				if err := json.Unmarshal([]byte(*input), in); err != nil {
-					return err
-				}
 			}
 
-			result := method.Call([]reflect.Value{
-				reflect.ValueOf(context.Background()),
-				reflect.ValueOf(in),
-			})
-			if len(result) != 2 {
-				panic("service methods should always return 2 values (error and result)")
-			}
-			if !result[1].IsNil() {
-				return result[1].Interface().(error)
-			}
-			out := result[0].Interface()
-			data, err := json.MarshalIndent(out, "", "    ")
-			if err != nil {
-				return err
-			}
-			fmt.Println(string(data))
-		}
 
-		return nil
-	}
-}
-
-func dial(addr string) (*grpc.ClientConn, error) {
-	return grpc.Dial(addr, grpc.WithInsecure())
-}
-		defer conn.Close()
-		c := newClient(conn)
-		cv := reflect.ValueOf(c)
-		method := cv.MethodByName(method)
-		if method.IsValid() {
-			result := method.Call([]reflect.Value{
-				reflect.ValueOf(context.Background()),
-			})
-			if len(result) != 2 {
-				panic("service methods should always return 2 values (error and result)")
-			}
-			if !result[1].IsNil() {
-				return result[1].Interface().(error)
-			}
-			out := result[0].Interface()
-			data, err := json.MarshalIndent(out, "", "    ")
-			if err != nil {
-				return err
-			}
-			fmt.Println(string(data))
-		}
-
-		return nil
-	}
-}
 // func dial connects to the server using a secure transport and insecure and then returns a grpc.ClientConn.
 func dial(addr string) (*grpc.ClientConn, error) {
 	var opts []grpc.DialOption
