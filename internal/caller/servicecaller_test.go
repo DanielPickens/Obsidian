dde419e88aca44a8375abf6dd57b8a5350ca8379
Daniel Pickens <dpickens1989@gmail.com>
2022-05-04 13:22:57 -0600

    Added func TestMarshalJSON_Any

--- internal/caller/servicecaller_test.go
+++ internal/caller/servicecaller_test.go
@@ -82,3 +82,46 @@ func TestMarshalJSON_AnyNotFound(t *testing.T) {
 	assert.Equal(t, aValue.TypeUrl, res.A.TypeURL)
 	assert.NotEmpty(t, res.A.Err, "err should not be empty")
 }
+
+// TestMarshalJSON_Any is a regression test to ensure that the Any type is properly marshaled
+func TestMarshalJSON_Any(t *testing.T) {
+	mdAny, err := desc.LoadMessageDescriptorForMessage((*any.Any)(nil))
+	require.NoError(t, err, "failed to load Any message descriptor")
+	md, err := builder.NewMessage("any").
+		AddField(builder.NewField("id", builder.FieldTypeInt32())).
+		AddField(builder.NewField("name", builder.FieldTypeString())).
+		AddField(builder.NewField("a", builder.FieldTypeImportedMessage(mdAny))).
+		Build()
+	require.NoError(t, err, "error building new message descriptor")
+
+	aValue := &any.Any{
+		TypeUrl: "test.protobuf.Any",
+		Value:   []byte{'1', '2', '3'},
+	}
+	m := dynamic.NewMessage(md)
+	m.SetFieldByName("id", int32(1))
+	m.SetFieldByName("name", "test")
+	m.SetFieldByName("a", aValue)
+
+	sc := NewServiceCaller(nil, JSON, JSON, nil)
+	b, err := sc.marshalMessage(m)
+	require.NoError(t, err)
+
+	res := struct {
+		ID   int
+		Name string
+		A    struct {
+			TypeURL string `json:"@type"`
+			Value   string
+			Err     string
+		}
+	}{}
+
+	err = json.Unmarshal(b, &res)
+	require.NoError(t, err)
+
+	assert.Equal(t, 1, res.ID)
+	assert.Equal(t, "test2", res.Name)
+	assert.Equal(t, base64.StdEncoding.EncodeToString(aValue.Value), res.A.Value)
+	assert.Equal(t, aValue.TypeUrl, res.A.TypeURL)
+	assert.Empty(t, res.A.Err, "err should not be empty")
