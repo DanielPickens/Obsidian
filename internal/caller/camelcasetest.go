c9b053333b28ada4180e078d56d54037d5bf1b53
Daniel Pickens <dpickens1989@gmail.com>
2022-05-04 13:15:07 -0600

    Added camelcasetest to test ToLowerCamelCase function

---
+++ internal/caller/camelcasetest.go
@@ -0,0 +1,28 @@
+package caller
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TestToLowerCamelCase tests the ToLowerCamelCase function in the caller package.
+func TestToLowerCamelCase(t *testing.T) {
+	tests := []struct {
+		name     string
+		text     string
+		expected string
+	}{
+		{name: "no_transform", text: "testString", expected: "testString"},
+		{name: "mixed_case", text: "test_Str", expected: "testStr"},
+		{name: "lower_case", text: "test_Str", expected: "testStr"},
+		{name: "multiple_occurances", text: "test_Str_str", expected: "testStrStr"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res := toLowerCamelCase(tt.text)
+			assert.Equal(t, tt.expected, res)
+		})
+	}
+}
