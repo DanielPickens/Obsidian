ec37a4cf2fc1b65ae6a2da9f1d72bc4ebcde720b
Daniel Pickens <dpickens1989@gmail.com>
2022-05-04 13:12:20 -0600

    Added camelcase to internal/caller

---
+++ internal/caller/camelcase.go
@@ -0,0 +1,33 @@
+package caller
+
+import (
+	"strings"
+	"unicode"
+)
+
+// toLowerCamelCase converts a string to lowerCamelCase
+func toLowerCamelCase(s string) string {
+	res := strings.Builder{}
+	capNext := false
+	for _, v := range []byte(s) {
+		if v >= 'A' && v <= 'Z' {
+			res.WriteByte(v)
+		}
+		if v >= '0' && v <= '9' {
+			res.WriteByte(v)
+		}
+		if v >= 'a' && v <= 'z' {
+			if capNext {
+				res.WriteRune(unicode.ToUpper(rune(v)))
+			} else {
+				res.WriteByte(v)
+			}
+		}
+
+		capNext = false
+		if v == '_' || v == ' ' || v == '-' {
+			capNext = true
+		}
+	}
+	return res.String()
+}
