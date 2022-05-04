883d3d68a2cc92be3f74e1fd75fc46f56104df0d
Daniel Pickens <dpickens1989@gmail.com>
2022-05-04 13:04:40 -0600

    commit on main

---
+++ internal/caller/servicecaller.go
@@ -0,0 +1,229 @@
+package caller
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+
+	"github.com/DanielPickens/Obsidian/internal/rpc"
+	"github.com/golang/protobuf/jsonpb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/connectivity"
+	"google.golang.org/grpc/status"
+
+	"github.com/jhump/protoreflect/desc"
+	"github.com/jhump/protoreflect/dynamic"
+)
+
+// Proto message format
+type MsgFormat int
+
+func (f MsgFormat) String() string {
+	switch f {
+	case Text:
+		return "text"
+	case JSON:
+		return "json"
+	default:
+		return "unknown"
+	}
+}
+
+func ParseMsgFormat(s string) MsgFormat {
+	if s == "text" {
+		return Text
+	}
+
+	return JSON
+}
+
+const (
+	JSON MsgFormat = iota
+	Text
+)
+
+type temporary interface {
+	Temporary() bool
+}
+
+// IsErrTransient returns true if err is t.
+func IsErrTransient(err error) bool {
+	te, ok := err.(temporary)
+	return ok && te.Temporary()
+}
+
+type callerError struct {
+	err error
+}
+
+func (e *callerError) Error() string {
+	return e.err.Error()
+}
+
+func (e *callerError) Temporary() bool {
+	code := status.Code(e.err)
+	return code != codes.Unavailable
+}
+
+func (e *callerError) Cause() error {
+	return e.err
+}
+
+func (e *callerError) Unwrap() error {
+	return e.Cause()
+}
+
+func newCallerError(err error) *callerError {
+	return &callerError{err}
+}
+
+type ServiceCaller struct {
+	connFact     *rpc.GrpcConnFactory
+	inMsgFormat  MsgFormat
+	outMsgFormat MsgFormat
+	fdescCache   *FileDescCache
+}
+
+func NewServiceCaller(connFact *rpc.GrpcConnFactory, inMsgFormat, outMsgFormat MsgFormat, fdescCache *FileDescCache) *ServiceCaller {
+	return &ServiceCaller{
+		connFact:     connFact,
+		inMsgFormat:  inMsgFormat,
+		outMsgFormat: outMsgFormat,
+		fdescCache:   fdescCache,
+	}
+}
+
+func (sc *ServiceCaller) CallStream(ctx context.Context, serviceTarget string, methodDesc *desc.MethodDescriptor, messages [][]byte, callOpts ...grpc.CallOption) (chan []byte, chan error) {
+	errChan := make(chan error, 1)
+	conn, err := sc.getConn(serviceTarget)
+	if err != nil {
+		errChan <- newCallerError(err)
+		return nil, errChan
+	}
+
+	sd := grpc.StreamDesc{
+		StreamName:    methodDesc.GetName(),
+		ServerStreams: methodDesc.IsServerStreaming(),
+		ClientStreams: methodDesc.IsClientStreaming(),
+	}
+
+	// fully qualified method name is needed here
+	methodName := fmt.Sprintf("/%s/%s", methodDesc.GetService().GetFullyQualifiedName(), methodDesc.GetName())
+	stream, err := conn.NewStream(ctx, &sd, methodName, callOpts...)
+	if err != nil {
+		errChan <- newCallerError(err)
+		return nil, errChan
+	}
+
+	result := make(chan []byte)
+
+	go func() {
+		for {
+
+			m := dynamic.NewMessage(methodDesc.GetOutputType())
+			err := stream.RecvMsg(m)
+			if err != nil {
+				if err != io.EOF {
+					errChan <- newCallerError(err)
+				} else {
+					close(errChan)
+				}
+
+				close(result)
+				break
+			}
+
+			resMsg, err := sc.marshalMessage(m)
+			if err != nil {
+				errChan <- err
+				close(result)
+				break
+			}
+			result <- resMsg
+		}
+	}()
+
+	for _, reqMsg := range messages {
+		msg := dynamic.NewMessage(methodDesc.GetInputType())
+
+		err := sc.unmarshalMessage(msg, reqMsg)
+		if err != nil {
+			errChan <- newCallerError(fmt.Errorf("invalid input %s: %w", sc.inMsgFormat.String(), err))
+			return nil, errChan
+		}
+
+		err = stream.SendMsg(msg)
+		// in case of EOF the real error should be discovered by stream.RecvMsg()
+		if err == io.EOF {
+			return nil, errChan
+		}
+
+		if err != nil {
+			errChan <- newCallerError(err)
+			return nil, errChan
+		}
+
+	}
+
+	if err := stream.CloseSend(); err != nil {
+		errChan <- newCallerError(err)
+	}
+
+	return result, errChan
+}
+
+// CallClientStream allows calling unary or client stream methods as they both return only a single result
+func (sc *ServiceCaller) CallClientStream(ctx context.Context, serviceTarget string, methodDesc *desc.MethodDescriptor, messages [][]byte, callOpts ...grpc.CallOption) ([]byte, error) {
+	if len(messages) == 0 {
+		return nil, newCallerError(errors.New("empty requests are not allowed"))
+	}
+
+	resultCh, errChan := sc.CallStream(ctx, serviceTarget, methodDesc, messages, callOpts...)
+	var result []byte
+	for {
+		select {
+		case r := <-resultCh:
+			if r != nil {
+				result = r
+			}
+		case err := <-errChan:
+			return result, err
+		}
+	}
+}
+
+func (sc *ServiceCaller) getConn(target string) (*grpc.ClientConn, error) {
+	conn, err := sc.connFact.GetConn(target)
+	if err != nil {
+		return nil, err
+	}
+
+	if conn.GetState() != connectivity.Ready {
+		conn.ResetConnectBackoff()
+	}
+
+	return conn, err
+}
+
+func (sc *ServiceCaller) marshalMessage(msg *dynamic.Message) ([]byte, error) {
+	if sc.outMsgFormat == Text {
+		return msg.MarshalText()
+	}
+
+	return msg.MarshalJSONPB(&jsonpb.Marshaler{
+		EmitDefaults: true,
+		Indent:       "  ",
+		OrigName:     true,
+		AnyResolver:  &anyResolver{sc.fdescCache},
+	})
+}
+
+func (sc *ServiceCaller) unmarshalMessage(msg *dynamic.Message, b []byte) error {
+	if sc.inMsgFormat == Text {
+		return msg.UnmarshalText(b)
+	}
+
+	return msg.UnmarshalJSON(b)
+}
---
+++ internal/caller/servicemeta.go
@@ -0,0 +1,90 @@
+package caller
+
+import (
+	"context"
+	"time"
+
+	"github.com/DanielPickens/Obsidian/internal/rpc"
+	"github.com/jhump/protoreflect/desc"
+	"github.com/jhump/protoreflect/grpcreflect"
+	rpb "google.golang.org/grpc/reflection/grpc_reflection_v1alpha"
+)
+
+type ServiceMetaData interface {
+	GetServiceMetaDataList(context.Context) ([]*ServiceMeta, error)
+}
+
+type serviceMetaData struct {
+	connFact *rpc.GrpcConnFactory
+	target   string
+	deadline int
+}
+
+type ServiceMeta struct {
+	Name    string
+	Methods []*desc.MethodDescriptor
+	File    *desc.FileDescriptor
+}
+
+// NewServiceMetaData returns new instance of ServiceMetaData
+// that reads service metadata by calling grpc Reflection service of the target
+func NewServiceMetaData(connFact *rpc.GrpcConnFactory, target string, deadline int) ServiceMetaData {
+	return &serviceMetaData{
+		connFact: connFact,
+		target:   target,
+		deadline: deadline,
+	}
+}
+
+func (s *serviceMetaData) GetServiceMetaDataList(ctx context.Context) ([]*ServiceMeta, error) {
+	conn, err := s.connFact.GetConn(s.target)
+	if err != nil {
+		return nil, err
+	}
+	rpbclient := rpb.NewServerReflectionClient(conn)
+	callctx, cancel := context.WithTimeout(ctx, time.Duration(s.deadline)*time.Second)
+	defer cancel()
+	rc := grpcreflect.NewClient(callctx, rpbclient)
+
+	services, err := rc.ListServices()
+	if err != nil {
+		defer rc.Reset()
+		return nil, err
+	}
+
+	res := make([]*ServiceMeta, len(services))
+	for i, svc := range services {
+		svcDesc, err := rc.ResolveService(svc)
+		// sometimes ResolveService throws an error
+		// when different proto files have different dependency protos named identically
+		// For example service1.proto has common_types.proto and service2.proto has the same dependency
+		// protoreflect library caches dependencies by name
+		// so if we get an error, we can just recreate Client to reset internal cache and try again
+		if err != nil {
+			rc.Reset()
+			// try only once here
+			rc = grpcreflect.NewClient(callctx, rpbclient)
+			svcDesc, err = rc.ResolveService(svc)
+			if err != nil {
+				defer rc.Reset()
+				return nil, err
+			}
+		}
+
+		svcData := &ServiceMeta{
+			File:    svcDesc.GetFile(),
+			Name:    svcDesc.GetFullyQualifiedName(),
+			Methods: svcDesc.GetMethods(),
+		}
+
+		for _, m := range svcData.Methods {
+			u := newJsonNamesUpdater()
+			u.updateJSONNames(m.GetInputType())
+			u.updateJSONNames(m.GetOutputType())
+		}
+		res[i] = svcData
+	}
+
+	defer rc.Reset()
+	return res, nil
+}
