16c3abdad80728d9869a894c16f197ed6f6f9b1f
Daniel Pickens <dpickens1989@gmail.com>
2022-05-09 14:44:56 -0600

    Added test case to duration_test

--- internal/client/duration_test.go
+++ internal/client/duration_test.go
@@ -106,6 +106,49 @@ func TestDurationBool(t *testing.T) {
 	}
 }
 
+func TestDurationUnmarshalJSON(t *testing.T) {
+	tests := []struct {
+		name      string
+		val       string
+		expected  time.Duration
+		expectErr bool
+	}{
+		{name: "parse empty", val: "", expectErr: true},
+		{name: "parse invalid", val: "oops", expectErr: true},
+		{name: "parse invalid format", val: "1hour", expectErr: true},
+		{name: "parse seconds", val: "15s", expected: 15 * time.Second},
+		{name: "parse default seconds", val: "5", expected: 5 * time.Second},
+
+		{name: "parse minutes", val: "5m", expected: 5 * time.Minute},
+		{name: "parse mixed", val: "1m10s", expected: 70 * time.Second},
+	}
+
+	for _, tt := range tests {
+
+		t.Run(tt.name, func(t *testing.T) {
+			var res time.Duration
+
+			err := res.UnmarshalJSON([]byte(tt.val))
+
+			if tt.expectErr {
+
+				assert.NotNil(t, err)
+			} else {
+
+				assert.Nil(t, err)
+
+				assert.Equal(t, tt.expected, res)
+
+			}
+
+		}
+
+		)
+	}
+}
+
+
+
 
 func TestDurationParseError(t *testing.T) {
 	_, err := ParseDuration("")
