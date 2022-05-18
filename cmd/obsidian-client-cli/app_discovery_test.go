c9163ae8e85c53c4eb54ec78d22fc4a3cd26a2f6
Daniel Pickens <dpickens1989@gmail.com>
2022-05-18 08:05:10 -0600

    commit on main

---
+++ cmd/obsidian-client-cli/app_discovery_test.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	app_testing "github.com/DanielPickens/obsidian-client-cli/internal/testing"
+	"github.com/jhump/protoreflect/grpcreflect"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDiscoverCommand(t *testing.T) {
+	app, err := newApp(&startOpts{
+		Target:        app_testing.TestServerAddr(),
+		Deadline:      15,
+		IsInteractive: false,
+		Discover:      true,
+		Service:       "TestService",
+	})
+
+	buf := &bytes.Buffer{}
+	app.w = buf
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	err = app.Start([]byte("{}"))
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	res := buf.String()
+
+	if !strings.Contains(res, "service TestService") {
+		t.Errorf("expected TestService service def, got %s", res)
+		return
+	}
+
+	services, err := grpcreflect.LoadServiceDescriptors(app_testing.TestServerInstance())
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	testSvc, ok := services["obsidian_client_cli.testing.TestService"]
+	require.True(t, ok, "grpc service not found")
+	for _, m := range testSvc.GetMethods() {
+		if !strings.Contains(res, "rpc "+m.GetName()) {
+			t.Errorf("expected %s method def, got %s", m.GetName(), res)
+			return
+		}
+	}
+
+	for _, msg := range testSvc.GetFile().GetMessageTypes() {
+		if !strings.Contains(res, "message "+msg.GetName()) {
+			t.Errorf("expected %s message def, got %s", msg.GetName(), res)
+			return
+		}
+	}
+}
+
+func TestDiscoverCommand_NoService(t *testing.T) {
+	app, err := newApp(&startOpts{
+		Target:        app_testing.TestServerAddr(),
+		Deadline:      15,
+		IsInteractive: false,
+		Discover:      true,
+		Service:       "",
+	})
+
+	buf := &bytes.Buffer{}
+	app.w = buf
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	err = app.Start([]byte("{}"))
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	res := buf.String()
+
+	if !strings.Contains(res, "service TestService") {
+		t.Errorf("expected TestService service def, got %s", res)
+		return
+	}
+}
